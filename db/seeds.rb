864f299b4ad80ebb193ae765671b1d743f85aa0c
Thiago Malaquias <tammalaquias@gmail.com>
2023-11-30 20:44:13 -0300

    restringindo ao upload de iamgns

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Administrador.find_or_create_by(nome: "danilo", email: "danilo@99run.com", senha: "123456")
 
 
-20.times do
-  Faker::Config.locale = 'pt-BR'
+# 20.times do
+#   Faker::Config.locale = 'pt-BR'
   
-  Usuario.create(
-    nome: Faker::Name.name,
-    email: Faker::Internet.email,
-    senha: "123456"
-  )
-end
+#   Usuario.create(
+#     nome: Faker::Name.name,
+#     email: Faker::Internet.email,
+#     senha: "123456"
+#   )
+# end
